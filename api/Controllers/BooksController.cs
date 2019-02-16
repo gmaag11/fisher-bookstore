e676f33557162ac5201c6beae6e6ae22129f84a7
Garrett Maag <gmaag11@hotmail.com>
2019-02-16 15:14:50 -0500

    Code to post new book to database

--- api/Controllers/BooksController.cs
+++ api/Controllers/BooksController.cs
@@ -49,7 +49,7 @@ namespace Fisher.Bookstore.Api.Controllers
             return Ok(db.Books);
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name = "GetBook")]
         public IActionResult GetBook(int id)
         {
             var book = db.Books.FirstOrDefault(b => b.Id == id);
@@ -60,6 +60,20 @@ namespace Fisher.Bookstore.Api.Controllers
             }
 
             return Ok(book);
+        }
+
+        [HttpPost]
+        public IActionResult Post ([FromBody]Book book)
+        {
+            if (book == null)
+            {
+                return BadRequest();
+            }
+
+            db.Books.Add(book);
+            db.SaveChanges();
+
+            return CreatedAtRoute("GetBook", new { id = book.Id }, book);
         }
     }
 }
