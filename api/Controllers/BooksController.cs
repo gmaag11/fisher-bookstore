6f4fe9b0b0512cdbae3ed91e5cd16d84320e3764
Garrett Maag <gmaag11@hotmail.com>
2019-02-16 15:34:57 -0500

    Delete a book

--- api/Controllers/BooksController.cs
+++ api/Controllers/BooksController.cs
@@ -100,5 +100,21 @@ namespace Fisher.Bookstore.Api.Controllers
 
             return NoContent();
         }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var book = db.Books.FirstOrDefault(b => b.Id == id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            db.Books.Remove(book);
+            db.SaveChanges();
+
+            return NoContent();
+        }
     }
 }
