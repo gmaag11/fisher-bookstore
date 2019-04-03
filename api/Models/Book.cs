f77e1e477054ac83bde7f1a9911d31e85ed45eb4
Garrett Maag <gmaag11@hotmail.com>
2019-04-02 23:56:20 -0400

    lab9

---
+++ api/Models/Book.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Fisher.Bookstore.Models
+{
+    public class Books
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public string ISBN { get; set; }
+
+        public DateTime publishDate { get; set; }
+
+        public string Publisher { get; set; }
+
+        public void ChangePublicationDate(DateTime dateTime)
+        {
+            this.PublishDate = dateTime;
+        }
+    }
+}
--- api/Startup.cs
+++ api/Startup.cs
@@ -27,7 +27,6 @@ namespace Fisher.Bookstore.Api
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
