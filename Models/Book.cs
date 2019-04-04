3493121fff85653084b1e521a90e473858dc1744
Garrett Maag <gmaag11@hotmail.com>
2019-04-03 20:37:03 -0400

    Readd Models and Migrations

---
+++ Models/Book.cs
@@ -0,0 +1,24 @@
+using System; 
+
+namespace Fisher.Bookstore.Models
+{
+    public class Book
+    {
+        public int Id { get; set; } // shortcut to build a property: "prop" + tab
+
+        public string Title { get; set; }
+
+        public Author Author { get; set; }
+
+        public string ISBN { get; set; }
+
+        public DateTime PublishDate { get; set; }
+
+        public string Publisher { get; set; }
+
+        public void ChangePublicationDate(DateTime dateTime)
+        {
+            this.PublishDate = dateTime;
+        }
+    }
+}
---
+++ api/Migrations/20190328024734_init.cs
@@ -0,0 +1,34 @@
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Fisher.Bookstore.Api.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Title = table.Column<string>(nullable: true),
+                    Author = table.Column<string>(nullable: true),
+                    ISBN = table.Column<string>(nullable: true),
+                    publicationDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
